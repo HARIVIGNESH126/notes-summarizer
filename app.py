29c9c67b1559f01fce71aa05be07910b34bad6fd
HARIVIGNESH126 <harivignesh126@gmail.com>
2026-07-08 10:46:07 +0530

    Updated for Render

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from operator import add
+
 import flask
 from flask_cors import CORS
 import PyPDF2
@@ -69,5 +71,10 @@ def summarize():
     return flask.jsonify({"summary": summary})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=False)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port) 
